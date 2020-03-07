76498347282542ca81a0b38be6c02b6bd5f38240
yichongyang <33671891+yichongyang@users.noreply.github.com>
2020-03-07 12:02:36 -0500

    Update myTestServer.js

--- myTestServer.js
+++ myTestServer.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome to my world');
+  res.send('Welcome to Yichong\'s world!');
 });
 
 app.listen(PORT, HOST);
