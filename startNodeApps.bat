845055d7fc1f4ede7c8e05d734d358fc446b019e
yichongyang <33671891+yichongyang@users.noreply.github.com>
2020-03-07 12:51:50 -0500

    Change the loop starting value

--- startNodeApps.bat
+++ startNodeApps.bat
@@ -10,7 +10,7 @@ rem docker rm -f $(docker ps -a -q --filter="name=yyang-node-app")
 
 set /a number=10000+%count%
 
-for /l %%x in (10000, 1, %number%) do (
+for /l %%x in (10001, 1, %number%) do (
   docker rm -f yyang-node-app-%%x > nul 2>&1 
   start /B docker run -p %%x:9000 --name yyang-node-app-%%x yyang/node-app > nul 2>&1
 )
