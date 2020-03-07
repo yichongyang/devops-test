6e61474c3f751466b121cd64a8867d526bf0e8fb
yichongyang <33671891+yichongyang@users.noreply.github.com>
2020-03-07 12:46:31 -0500

    Correct the port mapping

--- startNodeApps.bat
+++ startNodeApps.bat
@@ -8,10 +8,11 @@ if not "%1" == "" (
 
 rem docker rm -f $(docker ps -a -q --filter="name=yyang-node-app")
 
-for /l %%x in (1, 1, %count%) do (
-  docker rm -f yyang-node-app-%%x > nul 2>&1
-  set /a port=19000-%%x
-  start /B docker run -p %port%:9000 --name yyang-node-app-%%x yyang/node-app > nul 2>&1
+set /a number=10000+%count%
+
+for /l %%x in (10000, 1, %number%) do (
+  docker rm -f yyang-node-app-%%x > nul 2>&1 
+  start /B docker run -p %%x:9000 --name yyang-node-app-%%x yyang/node-app > nul 2>&1
 )
 
 exit 0
